9534df3b9b64f9b2fe8661a08b2aa0a49bdedf3c
madhurshinde <shindemadhur786@gmail.com>
2024-12-27 17:43:07 +0530

    add gitcheck

---
+++ src/test/java/playlistRunner/GitCheck.java
@@ -0,0 +1,5 @@
+package playlistRunner;
+
+public class GitCheck {
+
+}
--- src/test/java/playlistRunner/ProjectRunner.java
+++ src/test/java/playlistRunner/ProjectRunner.java
@@ -4,10 +4,12 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
+	
 		
+		// need changes added 
 		features= {"src\\test\\resources\\SpotifyFeature"}, 
 		
-		glue= {"steps"},
+		glue= {"steps,hooks"},
 		
 		plugin= {"pretty","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},
 				
